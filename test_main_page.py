63bdd5c7c4e93be1f1fadea406a9c2d94c7f5efc
Smith2Smith <zergan@yandex.ru>
2020-03-07 18:55:20 +0300

    Add test: test_guest_should_see_login_link

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,9 @@
 #Stepik_auto_test_FINAL_PROJ/test_main_page.py
 #test_main_page.py
 
+# ФАЙЛ С ТЕСТАМИ
+
+#Запуск :
 #pytest -v --tb=line --language=en test_main_page.py
 
 #--tb=line указывает, что нужно выводить только одну строку из лога каждого упавшего теста
@@ -10,8 +13,19 @@ from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, link)      # инициализируем Page Object, передаем в конструктор
+                                        # экземпляр драйвера и url адрес
+
+    page.open()                         # открываем страницу
+
+    page.go_to_login_page()             # выполняем метод страницы -
+                                        # переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()         # проверяем есть ли ссылка на логин
 
 #
