f4532401dc056a40f28107170d86f2443d808b17
Smith2Smith <zergan@yandex.ru>
2020-03-07 22:15:57 +0300

    Add locators.py

---
+++ pages/locators.py
@@ -0,0 +1,18 @@
+#Stepik_auto_test_FINAL_PROJ
+#pages/locators.py
+#locators.py
+
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FIELD_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_FIELD_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+
+    REGISTRATION_FIELD_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_FIELD_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_FIELD_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+
+#
