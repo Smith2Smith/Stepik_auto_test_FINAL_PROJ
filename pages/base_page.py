550966f2a1fbb88fdd44f472039ff90337a7ae3e
Smith2Smith <zergan@yandex.ru>
2020-03-08 20:19:57 +0300

    Add method solve_quiz_and_get_code

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,8 @@
 
 from selenium.webdriver import Remote as RemoteWebDriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 # конструктор — метод, который вызывается, когда мы создаем объект.
 # Конструктор объявляется ключевым словом __init__.
@@ -27,7 +29,19 @@ class BasePage():
             return False
         return True
 
-
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
 
 
 #
