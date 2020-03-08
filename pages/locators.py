0f174f0cb37052c71ce143c2b8a08934c85948d4
Smith2Smith <zergan@yandex.ru>
2020-03-08 14:22:28 +0300

    Add locators: LOGIN_FORM, REGISTRATION_FORM

--- pages/locators.py
+++ pages/locators.py
@@ -8,9 +8,11 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_FIELD_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
     LOGIN_FIELD_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
 
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form")
     REGISTRATION_FIELD_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTRATION_FIELD_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTRATION_FIELD_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
