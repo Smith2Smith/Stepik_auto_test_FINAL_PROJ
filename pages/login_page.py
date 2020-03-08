05aa9d4ef5f7475fa8e7408ecfc15918eec077a1
Smith2Smith <zergan@yandex.ru>
2020-03-08 15:05:55 +0300

    Add implementation:  def should_be_login_url  def should_be_login_form  def should_be_register_form

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,6 +6,7 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -14,16 +15,15 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         # проверить текущий url браузера , что в нем есть подстрока 'login'
-        assert True
+        assert "login" in self.browser.current_url, "In current URL not found substring 'login'"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        # #login_form
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        # #register_form
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
 
 #
