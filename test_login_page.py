9dd64b44521232ef8d4c0b627647b17a4c33a87e
Smith2Smith <zergan@yandex.ru>
2020-03-08 18:26:34 +0300

    Disabled file

--- test_login_page.py
+++ test_login_page.py
@@ -9,16 +9,15 @@
 
 #--tb=line указывает, что нужно выводить только одну строку из лога каждого упавшего теста
 
-from .pages.login_page import LoginPage
-
-link = "http://selenium1py.pythonanywhere.com/accounts/login/"
-
-def test_guest_can_go_to_login_page(browser):
-
-    page = LoginPage(browser, link)     # инициализируем Page Object, передаем в конструктор
-                                        # экземпляр драйвера и url адрес
-
-    page.open()                         # открываем страницу
-
-    page.should_be_login_page()         # выполняем метод страницы -
-                                        # проверяем страницу логина
+# from .pages.login_page import LoginPage
+#
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com"
+#
+#     page = LoginPage(browser, link)     # инициализируем Page Object, передаем в конструктор
+#                                         # экземпляр драйвера и url адрес
+#
+#     page.open()                         # открываем страницу
+#
+#     page.should_be_login_page()         # выполняем метод страницы -
+#                                         # проверяем страницу логина
