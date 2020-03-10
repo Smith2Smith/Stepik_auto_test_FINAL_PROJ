ddb5d78fe94617e1dfbf34682a2dcb84212b1bb6
Smith2Smith <zergan@yandex.ru>
2020-03-10 23:14:07 +0300

    Add methods: product_price product_name should_be_correct_price_basket should_be_correct_name_add_product_to_backet should_be_add_product_to_basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,7 +7,30 @@ from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
+        # добавить товар в корзину
         link = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         link.click()
+
+    def should_be_add_product_to_basket(self):
+        # проверка: сообщение о том, что товар добавлен в корзину
+        assert "has been added to your basket" in self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_HAS_BEEN_ADDED_TO_BASKET).text, "Product has been added to your basket"
+
+    def should_be_correct_name_add_product_to_backet(self, product_name):
+        # проверка: название товара в сообщении, совпадает с тем товаром, который добавили
+        assert product_name in self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_HAS_BEEN_ADDED_TO_BASKET).text, "Product name added to cart is incorrect"
+
+    def should_be_correct_price_basket(self, product_price):
+        # проверка: стоимость корзины совпадает с ценой товара
+        assert product_price == self.browser.find_element(*ProductPageLocators.PRICE_PRODUCT_HAS_BEEN_ADDED_TO_BASKET).text, "Product price added to cart is incorrect"
+
+    def product_name(self):
+        # название товара в карточке товара
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_PRODUCT_CARD).text
+        return product_name
+
+    def product_price(self):
+        # цена товара в карточке товара
+        product_price = self.browser.find_element(*ProductPageLocators.PRICE_IN_PRODUCT_CARD).text
+        return product_price
 
 #
