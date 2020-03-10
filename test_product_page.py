455bfe9071dfd7f334150b83fdb8a288d960a98f
Smith2Smith <zergan@yandex.ru>
2020-03-10 23:15:53 +0300

    Add check: should_be_correct_price_basket should_be_correct_name_add_product_to_backet should_be_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -9,15 +9,18 @@
 
 
 from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
 
 link ="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#link ="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-
-
+    page.should_be_add_product_to_basket()
+    page.should_be_correct_name_add_product_to_backet(page.product_name())
+    page.should_be_correct_price_basket(page.product_price())
 
 #
