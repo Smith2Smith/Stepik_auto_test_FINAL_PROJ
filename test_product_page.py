49099740285d4d03f4e3692206696ab25c7b0c17
Smith2Smith <zergan@yandex.ru>
2020-03-11 19:32:03 +0300

    Add tests: test_guest_should_see_login_link_on_product_page test_guest_can_go_to_login_page_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -6,6 +6,7 @@
 
 #Запуск :
 #pytest -v -s --tb=line --language=en test_product_page.py
+#pytest -m test1 -v -s --tb=line --language=en test_product_page.py
 
 from .pages.product_page import ProductPage
 import pytest
@@ -54,5 +55,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.should_be_success_message_disappeared()
+
+#@pytest.mark.test2
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+#@pytest.mark.test1
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
 
 #
