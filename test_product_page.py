beafadd1f07af65bb67ed795f73b3dd2e3fbf962
Smith2Smith <zergan@yandex.ru>
2020-03-11 23:20:03 +0300

    Add test:   test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -9,6 +9,7 @@
 #pytest -m test1 -v -s --tb=line --language=en test_product_page.py
 
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 
 #link ="http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
@@ -58,17 +59,26 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 #@pytest.mark.test2
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 #@pytest.mark.test1
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+#@pytest.mark.test1
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_no_product_in_basket()
+    basket_page.should_be_message_about_empty_basket()
 
 #
